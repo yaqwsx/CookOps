a7d7fd68a2830b9c3a97c3d5576ee7fcd49a0a40
Jan Mrázek <email@honzamrazek.cz>
2026-08-23 23:16:38 +0200

    Regenerate typed API client

--- frontend/src/api/generated.ts
+++ frontend/src/api/generated.ts
@@ -3,6 +3,7 @@
  * Do not make direct changes to the file.
  */
 
+
 export type paths = {
   "/api/v1/organizations": {
     /** Available Organizations */
@@ -788,9 +789,7 @@ export type components = {
     PushChangesResponse: {
       /** Change Cursor */
       change_cursor: string;
-      clock_skew_warning:
-        | components["schemas"]["ClockSkewWarningResponse"]
-        | null;
+      clock_skew_warning: components["schemas"]["ClockSkewWarningResponse"] | null;
       /** Outcomes */
       outcomes: components["schemas"]["PushCommandOutcomeResponse"][];
       /**
@@ -810,8 +809,8 @@ export type components = {
       code: string;
       /** Field Violations */
       field_violations: {
-        [key: string]: string;
-      }[];
+          [key: string]: string;
+        }[];
       /** Retry Same Identity */
       retry_same_identity: boolean;
     };
@@ -1122,6 +1121,7 @@ export type $defs = Record<string, never>;
 export type external = Record<string, never>;
 
 export type operations = {
+
   /** Available Organizations */
   available_organizations_api_v1_organizations_get: {
     responses: {
